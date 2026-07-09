f1d83cc1265419457aa2a686599e33903191d678
nikos2026 <nikos2026@126.com>
2026-07-09 16:15:36 +0800

    report去掉登录拦截

--- genesis_app/lib/components/common/genesis_report_actions.dart
+++ genesis_app/lib/components/common/genesis_report_actions.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../app/bootstrap/app_services_scope.dart';
-import '../auth/login_guard.dart';
 import 'genesis_content_submission_dialog.dart';
 
 const TextStyle _genesisActionMenuTextStyle = TextStyle(
@@ -845,7 +844,6 @@ Future<bool> showGenesisReportDialog({
   required String targetType,
   required String targetId,
 }) async {
-  if (!await ensureGenesisLogin(context)) return false;
   if (!context.mounted) return false;
   final api = AppServicesScope.read(context).api;
   return showGenesisContentSubmissionDialog(
