74ed545ed3a8a22d6fec3c8e3784a54f5f58ed2f
xiaoyi <xiaoyi@MacBook-Air.local>
2026-07-17 13:29:46 +0800

    优化buy gems页面的UI样式

--- genesis_app/lib/components/gems/gem_colors.dart
+++ genesis_app/lib/components/gems/gem_colors.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
 const Color kGemAccentColor = Color(0xFFFF2442);
+const Color kGemSoldOutBorderColor = Color(0xFFFFD1D8);
+const Color kGemSoldOutForegroundColor = Color(0xFFD47B89);
 const Color kGemTaskActionColor = Color(0xFFAD403B);
-const Color kGemTaskActionDisabledColor = Color(0xFFD6A09D);
+const Color kGemTaskClaimedForegroundColor = Color(0xFFD47B89);
+const Color kGemTaskProgressForegroundColor = Color(0xFF338960);
