2aa2d24e82437b699088532d230cd0a112146a94
nikos2026 <nikos2026@126.com>
2026-07-22 11:01:33 +0800

    optimization

--- genesis_app/lib/main.dart
+++ genesis_app/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 import 'app/bootstrap/app_bootstrap.dart';
+import 'app/config/app_config.dart';
 import 'app/config/app_endpoint_overrides.dart';
 import 'app/genesis_app.dart';
 import 'app/startup/app_startup_coordinator.dart';
@@ -19,7 +20,13 @@ Future<void> main() async {
       DeviceOrientation.portraitUp,
     ]),
   );
-  final appConfig = await AppEndpointOverrideStore.loadConfig();
+  final appConfig = await AppEndpointOverrideStore.loadConfig().timeout(
+    const Duration(seconds: 2),
+    onTimeout: () {
+      debugPrint('[Startup] endpoint override load timed out; using defaults');
+      return const AppConfig();
+    },
+  );
 
   void runGenesisApp() {
     final services = AppBootstrap.createInitialServices(config: appConfig);
